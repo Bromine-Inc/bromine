bdc2083758830861d550ea449db59dda3070372d
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-02 14:00:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const mineflayer = require("mineflayer")
 const http = require("http")
+
+function botHandler (req, res) {
+  // and then a miracle happens
+}
+
+http.createServer(botHandler).listen(8000);
