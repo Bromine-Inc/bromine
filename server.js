748e18c3aff0c89d3a4f2437e90cf0f77b239128
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-02 16:38:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const mineflayer = require("mineflayer")
-const http = require("http")
+const mineflayer = require("mineflayer") // Put a decent chunk of the workload on a package made by someone else
+const http = require("http") // We need a server to accept requests
+
+function databasePush {
+
+}
+
+function actionDecider (action, user) {
+
+}
 
 function botHandler (req, res) {
-  // and then a miracle happens
+  var [action,user] = req.url.slice(1).split('/', 2) // To do [ACTION] with a bot with id [ID] send a request to /[ACTION]/[USER]. // URL unknown at time of writing.
+  
 }
 
 http.createServer(botHandler).listen(8000);
