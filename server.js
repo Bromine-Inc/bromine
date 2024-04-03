4d4b3654c2a368a53b3a1336350f4404c1ff4f27
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-03 15:14:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const mineflayer = require("mineflayer"); // Put a decent chunk of the workload on a package made by someone else
+const mcprotocol = require("minecraft-protocol") // We need the class from here
+const classt = require("class-transformer") // We need to retrieve stuff from the database and convert it to a class
 const http = require("http"); // We need a server to accept requests
 const fApp = require("firebase/app"); // Get the firebase app
 const fAnl = require("firebase/analytics"); // Idk why we need analytics
 const firebaseConfig = { // Config stuff
-  apiKey: "AIzaSyDFFiw65z47zWeICeC3eRZhkcbE_pLU9BA",
+  apiKey: "AIzaSyDFFiw65z47zWeICeC3eRZhkcbE_pLU9BA", // For some reason it's ok to share the api key publicly
   authDomain: "b-romine.firebaseapp.com",
   projectId: "b-romine",
   storageBucket: "b-romine.appspot.com",
@@ -18,12 +20,15 @@ function databasePush () {
 // a miracle happens
 }
 
-function actionDecider (action, user) {
+function actionDecider (action, user, data) {
 // another miracle happens
 }
 
 function botHandler (req, res) {
-  var [action,user] = req.url.slice(1).split('/', 2) // To do [ACTION] with a bot with id [ID] send a request to /[ACTION]/[USER]. // URL unknown at time of writing.
+  var [action,user,data] = req.url.slice(1).split('/', 3) // To do [ACTION] with a bot with id [ID] with data [DATA] send a request to https://b-romine.web.app//[ACTION]/[USER]/[DATA] // Site not up yet  
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write('{"message": "Under Construction!"}'); // Will be implemented later
+  res.end();
   // another half a miracle happens
 }
 
