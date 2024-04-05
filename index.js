9d4325a5358eccb093c2cb558cfdfda9c7d1a48a
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-05 10:44:22 +0100

    Update index.js
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ var mcprotocol = require("minecraft-protocol"); // We need the class from here
 var classt = require("class-transformer"); // We need to retrieve stuff from the database and convert it to a class
 var func = require("firebase-functions"); // functions!
 var http = require("http"); // We need a server to accept requests
+var fApp = require("firebase/app"); // Get the firebase app
+var fAnl = require("firebase/analytics"); // Idk why we need analytics
+var fDat = require("firebase/database");
+var firebaseConfig = { // Config stuff
+  apiKey: "AIzaSyDFFiw65z47zWeICeC3eRZhkcbE_pLU9BA", // For some reason it's ok to share the api key publicly
+  authDomain: "b-romine.firebaseapp.com",
+  projectId: "b-romine",
+  storageBucket: "b-romine.appspot.com",
+  messagingSenderId: "663671114255",
+  appId: "1:663671114255:web:5dfbb8470b5a8ebc60b17a",
+  measurementId: "G-JP0HTVJ203",
+  databaseURL: "https://b-romine-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+var app = fApp.initializeApp(firebaseConfig); // Initialize!
+var analytics = fAnl.getAnalytics(app); // OMG IDK WHY I NEED THIS!!!
+const database = fDat.getDatabase(app); // It's time for some data!
 
 function databasePush() {
 // a miracle happens
