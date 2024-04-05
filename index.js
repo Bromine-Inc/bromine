e0e29ce77aa9430f7b4e5c20425ec901f9746ef5
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-05 10:40:29 +0100

    Update index.js
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ var mineflayer = require("mineflayer"); // Put a decent chunk of the workload on
 var mcprotocol = require("minecraft-protocol"); // We need the class from here
 var classt = require("class-transformer"); // We need to retrieve stuff from the database and convert it to a class
 var func = require("firebase-functions"); // functions!
-const http = require("http"); // We need a server to accept requests
+var http = require("http"); // We need a server to accept requests
 
 function databasePush() {
 // a miracle happens
 }
 
 function actionDecider(action, user, data) {
-// another miracle happens
+  console.log({'action': action, 'user': user, 'data': data});
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(`<h1>Hello!</h1><p>${`${action}/${user}/${data}`}</p>`); // Will be implemented later
+  res.end(); // It will think the request timed out if we don't do this!
 }
 
 function botHandler(req, res) {
-  var [action,user,data] = req.url.slice(1).split('/', 3) // To do [ACTION] with a bot with id [ID] with data [DATA] send a request to https://b-romine.web.app//[ACTION]/[USER]/[DATA] // Site not up yet 
-  console.log({'action': action, 'user': user, 'data': data})
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(`<h1>Hello!</h1><p>${`${action}/${user}/${data}`}</p>`); // Will be implemented later
+  var [action, user, data] = req.url.slice(1).split('/', 3); // To do [ACTION] with a bot with id [ID] with data [DATA] send a request to https://bromine-mw3o.onrender.com/[ACTION]/[USER]/[DATA] 
+  actionDecider(action, user, data)
   // another half a miracle happens
 }
 
