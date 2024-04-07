cad7cbcb2d1ad0e3a45a920a649b2a62bb3630e4
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-07 10:01:02 +0100

    Time to have a favicon.ico

--- index.js
+++ index.js
@@ -26,36 +26,42 @@ var [child, get] = [fDat.child, fDat.get];
 var [PtoC, ser] = [classt.plainToInstance, ((o) => {JSON.stringify(instanceToPlain(o))})];
 
 var html;
-fs.readFile('./index.html', ((err, data) => {html = data.toString(); if (err) {throw err};}))
+var img;
+fs.readFile('./index.html', ((err, data) => {html = data.toString(); if (err) {throw err};}));
+fs.readFile('./favicon.ico', ((err, data) => {img = data.toString(); if (err) {throw err};}));
 
 function readParse(id) {
   function parse(data) { // Sheesh
     let a = data
-    a._client.splitter.buffer = PtoC(Buffer, a._client.splitter.buffer)
-    a._client.splitter = PtoC(mcprotocol.framing.Splitter, a._client.splitter)
-    a._client.framer = PtoC(mcprotocol.framing.Framer, a._client.framer)
-    a._client = PtoC(mcprotocol.Client, a._client)
-    a = PtoC(events.EventEmitter, a) // Did you know the object returned by mineflayer.createBot() is just an EventEmitter?
-    return a
+    a._client.splitter.buffer = PtoC(Buffer, a._client.splitter.buffer);
+    a._client.splitter = PtoC(mcprotocol.framing.Splitter, a._client.splitter);
+    a._client.framer = PtoC(mcprotocol.framing.Framer, a._client.framer);
+    a._client = PtoC(mcprotocol.Client, a._client);
+    a = PtoC(events.EventEmitter, a); // Did you know the object returned by mineflayer.createBot() is just an EventEmitter?
+    return a;
   }
-  return get(child(fdref, id)).then(parse)
+  return get(child(fdref, id)).then(parse);
 }
 
 function actionDecider(action, user, data) {
   console.log({'action': action, 'user': user, 'data': data});
-  var status = 200
-  var ctype = 'application/json'
+  var status = 200;
+  var ctype = 'application/json';
   var response = {}
   switch (action) {     
     case "wakeup":
-      var response = {"message": "I'M SOOOOOOOOOOO TIRED"}
-      break
+      var response = {"message": "I'M SOOOOOOOOOOO TIRED"};
+      break;
+    case "favicon.ico":
+      var ctype = 'image/jpeg';
+      var response = img;
+      break;
     default:
-      var response = html
-      var ctype = 'text/html'
-      break
+      var response = html;
+      var ctype = 'text/html';
+      break;
   }
-  return [status, response, ctype]
+  return [status, response, ctype];
 }
 
 function botHandler(req, res) {
