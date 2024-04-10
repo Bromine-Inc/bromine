27f3039471b07d03ba27b307871ffb196f21f2f2
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-10 20:03:13 +0100

    add chunk support

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ function actionDecider(action, user, data) {
       response.data.messages = b.messages;
       if (clear) {bots[user].messages = []};
       response.data.online = b.online
+      response.data.position = b.entity.position
+      response.data.chunk = bot.world.getColumn(b.entity.position.x >> 4, b.entity.position.z >> 4)
       //response.data.world = b.world.getColumns();
       //response.data.players = b.players;
       //response.data.player = b.player;
